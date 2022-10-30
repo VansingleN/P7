9bba6a89527197365b3592e763efb8021a17e4ee
Nicolas <vansingle.n@gmail.com>
2022-10-30 17:23:48 +0100

    Finition du composant collapse + fichier collapseContent.json

---
+++ src/components/Collapse/Collapse.js
@@ -0,0 +1,26 @@
+import "./Collapse.css"
+import { useState } from "react"
+import vector from "../../assets/vector.png"
+
+
+function Collapse({ title, content }) {
+    const [isOpen, setIsOpen] = useState(false)
+    return isOpen ? (
+        <div className="collapse_open">
+            <button className="collapse_button" onClick={() => setIsOpen(false)}>
+                {title}
+                <img src={vector} alt="flèche d'ouverture" />
+            </button>
+            <p>{content}</p>
+        </div>
+    ) : (
+        <div className="collapse_closed">
+            <button className="collapse_button" onClick={() => setIsOpen(true)}>
+                {title}
+                <img src={vector} alt="flèche d'ouverture" />
+            </button>
+        </div>
+    )
+}
+
+export default Collapse;
--- src/screens/Apropos/Apropos.js
+++ src/screens/Apropos/Apropos.js
@@ -1,12 +1,22 @@
 import './Apropos.css'
 import Banner from "../../components/Banner/Banner"
 import banner_img_apropos from "../../assets/banner_img_apropos.png"
+import Collapse from '../../components/Collapse/Collapse'
+import * as collapseContent from "../../data/collapseContent.json"
+
+const collapseContentArray = collapseContent.default
 
 function Apropos() {
     return (
         <div>
             <Banner image = {banner_img_apropos}
-            text = "" />
+                text = "" />
+
+            {collapseContentArray.map(({ title, id, content }) => (
+                <Collapse title = {title}
+                    id = {id}
+                    content = {content} />
+            ))}
         </div>
     )
 }
