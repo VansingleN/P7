212a80406fee561c243d8305a1c34e227e88d925
Nicolas <vansingle.n@gmail.com>
2022-10-30 18:44:21 +0100

    Finalisation page d'erreur

--- src/components/Collapse/Collapse.js
+++ src/components/Collapse/Collapse.js
@@ -6,18 +6,18 @@ import vector from "../../assets/vector.png"
 function Collapse({ title, content }) {
     const [isOpen, setIsOpen] = useState(false)
     return isOpen ? (
-        <div className="collapse_open">
-            <button className="collapse_button" onClick={() => setIsOpen(false)}>
+        <div className = "collapse_open">
+            <button className = "collapse_button" onClick = {()  => setIsOpen(false)}>
                 {title}
-                <img src={vector} alt="flèche d'ouverture" />
+                <img src = {vector} alt = "flèche d'ouverture" />
             </button>
             <p>{content}</p>
         </div>
     ) : (
-        <div className="collapse_closed">
-            <button className="collapse_button" onClick={() => setIsOpen(true)}>
+        <div className = "collapse_closed">
+            <button className = "collapse_button" onClick = {()  => setIsOpen(true)}>
                 {title}
-                <img src={vector} alt="flèche d'ouverture" />
+                <img src = {vector} alt = "flèche d'ouverture" />
             </button>
         </div>
     )
--- src/screens/Error/Error.js
+++ src/screens/Error/Error.js
@@ -1,6 +1,11 @@
-function Error () {
-    return <div>
-        <h1>Salut c'est Error</h1>
+import { Link } from 'react-router-dom'
+import "./Error.css"
+
+function Error() {
+    return <div className = "error">
+        <h1 className = "error_404">404</h1>
+        <h2 className = "error_msg">Oups! La page que vous demandez n'existe pas.</h2>
+        <h3 className = "error_redirection"><Link to = "/">Retourner sur la page d'accueil</Link></h3>
     </div>
 }
 
